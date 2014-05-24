efb49ae18240cde52ea974a9303839aee9c80851
Pat Howell <patrick.howell@outlook.com>
2014-05-24 21:57:45 +0100

    Added handling of arguments from the command line when the program is run

--- CsvAndXmlConverter/CsvAndXmlConverter.cs
+++ CsvAndXmlConverter/CsvAndXmlConverter.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CsvAndXmlConverter
 {
-    class CsvAndXmlConverter
+    public class CsvAndXmlConverter
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if (args.Length != 1 || args[0] == "?")
+            {
+                ShowUsageMessage();
+            }
+            else
+            {
+                HandleRequiredAction(args[0]);
+            }
+
+        }
+
+        private static void HandleRequiredAction(string command)
+        {
+            var extension = Path.GetExtension(command);
+            if (extension == "csv" || extension == "xml")
+            {
+                DetermineRequiredActionFromFileExtenstion(command, extension);
+            }
+            else
+            {
+                ShowUsageMessage();
+            }
+
+        }
+
+        private static void DetermineRequiredActionFromFileExtenstion(string path, string extension)
+        {
+            if (extension == "csv")
+            {
+                //csv to xml conversion
+            }
+            else
+            {
+                //xml to csv conversion
+            }
+        }
+
+        private static void ShowUsageMessage()
+        {
+            Console.WriteLine();
         }
     }
 }
