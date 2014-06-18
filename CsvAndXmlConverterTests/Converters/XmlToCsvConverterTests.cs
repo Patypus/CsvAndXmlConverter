5bcee3696f5ac909361c8f0f145c0faba1393e76
Pat Howell <patrick.howell@outlook.com>
2014-06-18 22:06:56 +0100

    Added test class for xml to csv converter and added failing tests to define functionality of converter

---
+++ CsvAndXmlConverterTests/Converters/XmlToCsvConverterTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvAndXmlConverterTests.Converters
+{
+    [TestClass]
+    public class XmlToCsvConverterTests
+    {
+        [TestMethod]
+        public void TestCorrectFilePathIsRequestedToBeReadFromFileReaderByConverter()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestConvertedFileIsPassedToFileWriterWithCorrectName()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestARowIsAddedForEachChildElementOfTheRootElement()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestCommonElementTagsBecomeColumnTitles()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestElementValuesAreInCorrectColumnsAfterConversion()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestFullContentOfConvertedFileIsCorrect()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestUnMatchingElemetIsHandledCorrectly()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestFileWithNoDataIsHandledCorrectly()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestFileWithNoChildTagsIsHandledCorrectly()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestMissingFileErrorIsReportedByConverter()
+        {
+            Assert.IsFalse(true);
+        }
+
+        [TestMethod]
+        public void TestMissingDirectoryErrorIsReportedByConverter()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
