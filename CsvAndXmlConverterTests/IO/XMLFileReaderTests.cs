c0c7bd8e4b8c20a93c6c4e6692cb83ffd3b70809
Pat Howell <patrick.howell@outlook.com>
2014-06-18 22:19:53 +0100

    Added interface ofr XML file loader, added implementing class and failing unit tests to define the functionality of the class.

---
+++ CsvAndXmlConverterTests/IO/XMLFileReaderTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvAndXmlConverterTests.IO
+{
+    [TestClass]
+    public class XMLFileReaderTests
+    {
+        [TestMethod]
+        public void TestReaderReturnsContentFromValidFile()
+        {
+            Assert.IsFalse(true);
+        }
+
+        /*
+         * These exceptions are specifically tested for as they are used to provide
+         * specific error messages to the user. Any changes to StandardFileReader must
+         * still satisfy these tests.
+         */
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TestIncorrectFileNameThrowsExpectedException()
+        {
+            Assert.IsFalse(true);
+        }
+
+         [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void TestInvalidPathThrowsExpectedException()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
