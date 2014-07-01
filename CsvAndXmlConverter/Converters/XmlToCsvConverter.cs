8f8b536bcac79644d154200e08a71b68f077c4a6
Pat Howell <patrick.howell@outlook.com>
2014-07-01 21:56:40 +0100

    Added unit tests for file handling calls from xml to csv converter and added code to pass the tests.

--- CsvAndXmlConverter/Converters/XmlToCsvConverter.cs
+++ CsvAndXmlConverter/Converters/XmlToCsvConverter.cs
@@ -1,17 +1,60 @@
 ﻿using CsvAndXmlConverter.Data;
+using CsvAndXmlConverter.IO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CsvAndXmlConverter.Converters
 {
     public class XmlToCsvConverter : IConverter
     {
+        private readonly IFileWriter _writer;
+        private readonly IXMLFileReader _reader;
+
+        public XmlToCsvConverter(IFileWriter writer, IXMLFileReader reader)
+        {
+            _writer = writer;
+            _reader = reader;
+        }
+
         public IConversionResult ConvertFile(string path)
         {
-            throw new NotImplementedException();
+            var documentToConvert = _reader.ReadDataFromFile(path);
+            var convertedContent = ConvertXmlContentToCSV(documentToConvert);
+            var writeResult = WriteDataToFile(convertedContent, CreatePathForConvertedFile(path));
+            return new ConversionResult { Success = true, ResultMessage = "success" };
+        }
+
+        private string ConvertXmlContentToCSV(XDocument content)
+        {
+            //TODO - add the conversion logic in here.
+            return content.ToString();
+        }
+
+        private string WriteDataToFile(string data, string path)
+        {
+            return _writer.SaveDataToFile(TranscribeDataStringToStream(data), path);
+        }
+
+        private MemoryStream TranscribeDataStringToStream(string data)
+        {
+            var dataStream = new MemoryStream();
+            var dataWriter = new StreamWriter(dataStream);
+            dataWriter.Write(data);
+            dataWriter.Flush();
+            dataStream.Position = 0;
+            return dataStream;
+        }
+
+        private string CreatePathForConvertedFile(string originalPath)
+        {
+            var filePath = Path.GetDirectoryName(originalPath);
+            var newFileName = "converted_" + Path.GetFileNameWithoutExtension(originalPath) + ".csv";
+            return Path.Combine(filePath, newFileName);
         }
     }
 }
--- CsvAndXmlConverterTests/Converters/XmlToCsvConverterTests.cs
+++ CsvAndXmlConverterTests/Converters/XmlToCsvConverterTests.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CsvAndXmlConverter.Converters;
+using CsvAndXmlConverter.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CsvAndXmlConverterTests.Converters
 {
@@ -13,13 +18,23 @@ namespace CsvAndXmlConverterTests.Converters
         [TestMethod]
         public void TestCorrectFilePathIsRequestedToBeReadFromFileReaderByConverter()
         {
-            Assert.IsFalse(true);
+            var path = @"C:\Test\This\path\isPassed\toTheReader.xml";
+            var mockWriter = CreateBasicFileWriterAcceptingAnyParametersAndReturningDummySuccessMessage();
+            var mockReader = CreateFileReaderReturingBasicDummyXmlConent();
+            var converter = new XmlToCsvConverter(mockWriter.Object, mockReader.Object);
+            converter.ConvertFile(path);
+            mockReader.Verify(mock => mock.ReadDataFromFile(path), Times.Exactly(1));
         }
 
         [TestMethod]
         public void TestConvertedFileIsPassedToFileWriterWithCorrectName()
         {
-            Assert.IsFalse(true);
+            var mockWriter = CreateBasicFileWriterAcceptingAnyParametersAndReturningDummySuccessMessage();
+            var mockReader = CreateFileReaderReturingBasicDummyXmlConent();
+            var converter = new XmlToCsvConverter(mockWriter.Object, mockReader.Object);
+            converter.ConvertFile(@"C:\Location\file.xml");
+            mockWriter.Verify(mock =>
+                mock.SaveDataToFile(It.IsAny<MemoryStream>(), @"C:\Location\converted_file.csv"), Times.Exactly(1));
         }
 
         [TestMethod]
@@ -74,6 +89,43 @@ namespace CsvAndXmlConverterTests.Converters
         public void TestMissingDirectoryErrorIsReportedByConverter()
         {
             Assert.IsFalse(true);
+        }
+
+        private Mock<IFileWriter> CreateBasicFileWriterAcceptingAnyParametersAndReturningDummySuccessMessage()
+        {
+            var mockWriter = new Mock<IFileWriter>();
+            mockWriter.Setup(mock => mock.SaveDataToFile(It.IsAny<MemoryStream>(), It.IsAny<string>())).Returns("Success");
+            return mockWriter;
+        }
+
+        private Mock<IXMLFileReader> CreateFileReaderReturingBasicDummyXmlConent()
+        {
+            var content = CreateBasicXMLStructure();
+            var reader = new Mock<IXMLFileReader>();
+            reader.Setup(mock => mock.ReadDataFromFile(It.IsAny<string>())).Returns(content);
+            return reader;
+        }
+
+        private XDocument CreateBasicXMLStructure()
+        {
+            var document = new XDocument();
+            var dummyDataRoot = new XElement("items");
+            var dummyItemOne = CreateBasicChildElement(1);
+            var dummyItemTwo = CreateBasicChildElement(2);
+            document.Add(dummyDataRoot);
+            return document;
+        }
+
+        private XElement CreateBasicChildElement(int index)
+        {
+            var element = new XElement("item");
+            var valueOne = new XElement("value1");
+            var valueTwo = new XElement("value2");
+            valueOne.Value = "item" + index + "content1";
+            valueTwo.Value = "item" + index + "content2";
+            element.Add(valueOne);
+            element.Add(valueTwo);
+            return element;
         }
     }
 }
